15b04f14f6dc84209ee18d956d76c89223fdd018
SolaryKryptic <ayushdat2007@gmail.com>
2026-08-01 15:50:44 +1200

    change to mean + 1 stdv

--- demand_estimation.py
+++ demand_estimation.py
@@ -36,14 +36,10 @@ def main():
 		lambda s: s.fillna(s.median())
 	)
 
-	means = (
-		train_used.groupby(["Supermarket", "DayOfWeek"])["Demand"]
-		.mean()
-		.unstack(fill_value=0)
-		.reindex(columns=WEEKDAYS)
-		.fillna(0.0)
-	)
-	est = np.ceil(means).astype(int)
+	stats = train_used.groupby(["Supermarket", "DayOfWeek"])["Demand"]
+	means = stats.mean().unstack(fill_value=0).reindex(columns=WEEKDAYS).fillna(0.0)
+	stds = stats.std().unstack(fill_value=0).reindex(columns=WEEKDAYS).fillna(0.0)
+	est = np.ceil(means + stds).astype(int)
 	est = est.merge(locations, on="Supermarket").set_index("Supermarket")
 	est["Total"] = est[WEEKDAYS[:-1]].sum(axis=1)
 
